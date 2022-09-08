6dbbdac77ad174d0a63390898973cd85707d90a8
Michele <63038410+MickTK@users.noreply.github.com>
2022-09-08 15:19:34 +0200

    -

--- More Breeding Stuff/src/updater.rb
+++ More Breeding Stuff/src/updater.rb
@@ -2,10 +2,13 @@ module MoreBreedingStuff
   def self.check_for_updates()
     name = "More Breeding Stuff"
     link = "https://raw.githubusercontent.com/MickTK/Pokemon-Essentials-Plugins/main/versions.json"
-    data = HTTPLite::JSON.parse(pbDownloadToString(link))
-    if data[name]["version"] > PluginManager.version(name)
-      message = "UPDATE: A new version of \"#{name}\" is available! Download it here: #{data[name]["link"]}"
-      puts "\e[32m#{message}\e[0m"
+    begin
+      data = HTTPLite::JSON.parse(pbDownloadToString(link))
+      if data[name]["version"] > PluginManager.version(name)
+        message = "UPDATE: A new version of \"#{name}\" is available! Download it here: #{data[name]["link"]}."
+        puts "\e[32m#{message}\e[0m"
+      end
+    rescue Exception
     end
   end
   MoreBreedingStuff.check_for_updates
