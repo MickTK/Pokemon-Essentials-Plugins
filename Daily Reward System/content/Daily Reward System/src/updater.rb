9648b71933ac38e89bbc103f03c6a07957264f0e
Michele <63038410+MickTK@users.noreply.github.com>
2023-07-04 19:59:30 +0200

    -
    Removed the updater from all the plugins.

--- Daily Reward System/content/Daily Reward System/src/updater.rb
+++
@@ -1,8 +0,0 @@
-def check_for_updates()
-  begin
-    response = pbDownloadToString("https://raw.githubusercontent.com/MickTK/Pokemon-Essentials-Plugins/main/Daily%20Reward%20System/meta.json")
-    response = HTTPLite::JSON.parse(response)
-    puts "\e[32m#{response["message"]}\e[0m" if response["version"] > PluginManager.version("Daily Reward System")
-  rescue; return; end
-end
-check_for_updates
